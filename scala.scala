e51cfceaac754abb1e6e78f3ba173b847efd01e9
Ruta <ruta.grigorjeva@gmail.com>
2020-08-09 20:56:05 +0200

    Food

---
+++ scala.scala
@@ -0,0 +1,12 @@
+package FoodCalculator
+
+object scala extends App {
+  val food = "potatoes"
+  val price = 0.37
+  val quantity = 20
+
+  println(s"It will cost $price to buy $quantity kilos of $food")
+
+}
+
+
